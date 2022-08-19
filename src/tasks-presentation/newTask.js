e96d1150f93ae625a552b030763fde99d6bb67f4
marco-bot-18 <marco.elloyd.consolacion18@gmail.com>
2022-08-19 16:44:36 +0800

    another commit

--- src/tasks-presentation/newTask.js
+++ src/tasks-presentation/newTask.js
@@ -18,7 +18,7 @@ export default function NewTask({ newTask, handleChange, handleSubmit }) {
             value={newTask.description || ""}
             onChange={handleChange}
           />
-          <Button variant="primary" type="submit">Add Task</Button>
+          <Button variant="success" type="submit">Add Task</Button>
         </>
       )}
     </form>
